78b65f028ad5a196089b8899650fa4a97183e762
Tan Teng Xing <seventan1234@gmail.com>
2023-11-28 22:07:21 +0800

    update navbar

--- lib/modules.ts
+++ lib/modules.ts
@@ -6,7 +6,6 @@ export type Item = {
 
 export const modules: { name: string; items: Item[] }[] = [
   {
-    // product catalogue, shipment, order, payment
     name: 'Supply Chain Management',
     items: [
       {
@@ -24,11 +23,6 @@ export const modules: { name: string; items: Item[] }[] = [
         name: 'Order ',
         slug: 'order',
         description: 'Manage order.',
-      },
-      {
-        name: 'Shipment',
-        slug: 'shipment',
-        description: 'Manage shipment status.',
       },
     ],
   },
@@ -52,84 +46,4 @@ export const modules: { name: string; items: Item[] }[] = [
       },
     ],
   },
-
-  // },
-  // {
-  //   name: 'File Conventions',
-  //   items: [
-  //     {
-  //       name: 'Loading',
-  //       slug: 'loading',
-  //       description:
-  //         'Create meaningful Loading UI for specific parts of an app',
-  //     },
-  //     {
-  //       name: 'Error',
-  //       slug: 'error-handling',
-  //       description: 'Create Error UI for specific parts of an app',
-  //     },
-  //     {
-  //       name: 'Not Found',
-  //       slug: 'not-found',
-  //       description: 'Create Not Found UI for specific parts of an app',
-  //     },
-  //   ],
-  // },
-  // {
-  //   name: 'Data Fetching',
-  //   items: [
-  //     {
-  //       name: 'Streaming with Suspense',
-  //       slug: 'streaming',
-  //       description:
-  //         'Streaming data fetching from the server with React Suspense',
-  //     },
-  //     {
-  //       name: 'Static Data',
-  //       slug: 'ssg',
-  //       description: 'Generate static pages',
-  //     },
-  //     {
-  //       name: 'Dynamic Data',
-  //       slug: 'ssr',
-  //       description: 'Server-render pages',
-  //     },
-  //     {
-  //       name: 'Incremental Static Regeneration',
-  //       slug: 'isr',
-  //       description: 'Get the best of both worlds between static & dynamic',
-  //     },
-  //   ],
-  // },
-  // {
-  //   name: 'Components',
-  //   items: [
-  //     {
-  //       name: 'Client Context',
-  //       slug: 'context',
-  //       description:
-  //         'Pass context between Client Components that cross Server/Client Component boundary',
-  //     },
-  //   ],
-  // },
-  // {
-  //   name: 'Misc',
-  //   items: [
-  //     {
-  //       name: 'Client Component Hooks',
-  //       slug: 'hooks',
-  //       description: 'Preview the routing hooks available in Client Components',
-  //     },
-  //     {
-  //       name: 'CSS and CSS-in-JS',
-  //       slug: 'styling',
-  //       description: 'Preview the supported styling solutions',
-  //     },
-  //     {
-  //       name: 'Code Snippets',
-  //       slug: 'snippets',
-  //       description: 'A collection of useful App Router code snippets',
-  //     },
-  //   ],
-  // },
 ];
