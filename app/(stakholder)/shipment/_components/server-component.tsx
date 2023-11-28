fa71c88db54b4bbe713d18454615ca7b417f34a3
Tan Teng Xing <seventan1234@gmail.com>
2023-11-28 22:02:06 +0800

    remove shipment

--- app/(stakholder)/shipment/_components/server-component.tsx
+++
@@ -1,9 +0,0 @@
-'use server'
-
-import { getRegistrationNo } from "app/api/action/getDrug"
-
-export default async function ServerComponent({ contract }: { contract: object }) {
-    const data = await getRegistrationNo(contract)
-
-    return <p>testing server component: {String(data)}</p>
-}
