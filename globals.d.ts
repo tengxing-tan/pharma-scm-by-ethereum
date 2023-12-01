15bfe263e5e1d57e10bba7b98c81008a8dd1fe81
Tan Teng Xing <seventan1234@gmail.com>
2023-12-01 17:31:08 +0800

    fix window.ethereum ts error

--- globals.d.ts
+++ globals.d.ts
@@ -1,6 +1,8 @@
 // next file should not be edited - you can create another declaration file like globals.d.ts
+import { MetaMaskInpageProvider } from "@metamask/providers";
+
 declare global {
   interface Window {
-    ethereum: any;
+    ethereum?: MetaMaskInpageProvider
   }
 }
